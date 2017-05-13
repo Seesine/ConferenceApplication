e4a9f53f39c9703ae79600b575d8de56df62c2a4
MatyasM12 <mihai_matyas@yahoo.com>
2017-05-13 15:47:57 +0300

    Merge branch 'master' of C:\Users\mikha\IdeaProjects\ConferenceApplication2 with conflicts.

---
+++ src/main/java/repository/FileRepository.java
@@ -0,0 +1,22 @@
+package repository;
+
+import model.File;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mikha on 5/13/2017.
+ */
+public class FileRepository {
+    private Connection dbConnection;
+    public FileRepository(Connection connection) {
+        this.dbConnection = connection;
+    }
+    List<File> fileList = new ArrayList<File>();
+
+    public void save(File f){
+        fileList.add(f);
+    }
+}
