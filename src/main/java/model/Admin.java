a3d167136c923ec33ebd170648c9bb7227b32062
Aciubotaritei Silviu-Cosmin <uttx1990@gmail.com>
2017-05-13 16:18:01 +0300

    Admin model added.

---
+++ src/main/java/model/Admin.java
@@ -0,0 +1,40 @@
+package model;
+
+/**
+ * Created by Cosmin on 5/13/2017.
+ */
+public class Admin {
+    public Admin(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private int id;
+    private String username;
+    private String password;
+}
