94003a5ea7f515bbe5fa567de55f479867dfe795
Aciubotaritei Silviu-Cosmin <uttx1990@gmail.com>
2017-05-16 07:55:11 +0300

    Hibernate 'admin' repository added. 'File' hbm config column modified according new database schema.

--- src/main/java/repository/AdminRepository.java
+++ src/main/java/repository/AdminRepository.java
@@ -1,19 +1,46 @@
 package repository;
 
+import model.Admin;
+import model.CM;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import java.sql.Connection;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Dragos on 5/8/2017.
  */
-public class AdminRepository {
-    SessionFactory factory;
+public class AdminRepository implements CRUDRepository {
+    private SessionFactory factory;
     public AdminRepository(SessionFactory factory)
     {
-
+        this.factory = factory;
     }
     public boolean login(String username, String password) {
-        return true;
+        boolean ret = false;
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Query query = session.createQuery("FROM Admin where username = :username and password = :password");
+            query.setParameter("username", username);
+            query.setParameter("password", password);
+            List<Admin> adminList = query.list();
+            if (!adminList.isEmpty())
+                ret = true;
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return ret;
     }
 }
