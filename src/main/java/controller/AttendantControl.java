15d1162a9aab63ba64f5d7f0f6926a6c220951ee
Seesine <see.sine@hotmail.com>
2017-05-22 17:50:51 +0300

    added attendant repo

--- src/main/java/controller/AttendantControl.java
+++ src/main/java/controller/AttendantControl.java
@@ -1,7 +1,15 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import main.Main;
+import model.Conference;
+import repository.ConfRepository;
+import model.Sections;
+
+import javax.swing.text.TableView;
 
 /**
  * Created by Dragos on 5/8/2017.
@@ -9,13 +17,26 @@ import main.Main;
 public class AttendantControl
 {
     final Main loginManager;
-    public AttendantControl(final Main loginManager)
-    {
+    private ConfRepository confRepo = new ConfRepository();
+    public AttendantControl(final Main loginManager) {
         this.loginManager = loginManager;
+        //this.confRepo = confRepo;
     }
     @FXML
     public void setLogoutAction()
     {
         loginManager.logOut();
+    }
+
+    @FXML
+    ComboBox<Conference> conferenceComboBox = new ComboBox<>();
+
+    public void initialize(){
+        conferenceComboBox.getItems().clear();
+        ObservableList<Conference> obs = FXCollections.observableArrayList(confRepo.getAll());
+        conferenceComboBox.setItems(obs);
+        //conferenceComboBox.getItems().addAll(confRepo.getAll());
+        conferenceComboBox.setPromptText("Conferinta");
+        conferenceComboBox.setEditable(true);
     }
 }
